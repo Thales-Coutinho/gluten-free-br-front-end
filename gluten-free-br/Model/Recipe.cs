2df7203a0ec2cc7834cbebd30d9fe0872c3c5969
Thales Martim <thales9696@protonmail.com>
2024-07-30 09:29:33 -0300

    creating pages

---
+++ gluten-free-br/Model/Recipe.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gluten_free_br.Model
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
